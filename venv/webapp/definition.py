a914cff99a9da1b28a278585336d844b083d1727
Abd AlRhman AlQiam <Abdurrahman.AlQiam@gmail.com>
2022-04-04 15:36:01 +0300

    Putting it all together

---
+++ venv/webapp/definition.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class Definition:
+
+    def __init__(self, term):
+        self.term = term
+
+    def get(self):
+        df = pd.read_csv('data.csv')
+        definitions = tuple(df.loc[df['word'] == self.term]['definition'])
+        return definitions
--- venv/webapp/main.py
+++ venv/webapp/main.py
@@ -1,8 +1,10 @@
 import justpy as jp
 from about import About
 from home import Home
+from dictionary import Dictionary
 
 
 jp.Route(About.path, About.serve)
 jp.Route(Home.path, Home.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 jp.justpy(port=8001)
