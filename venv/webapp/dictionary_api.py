5efacf551ba3da14290b7dfcd777a5e79b52ad54
Abd AlRhman AlQiam <Abdurrahman.AlQiam@gmail.com>
2022-04-08 15:14:17 +0300

    Running using API

---
+++ venv/webapp/dictionary_api.py
@@ -0,0 +1,35 @@
+import justpy as jp
+from definition import Definition
+import requests
+
+
+class Dictionary:
+    path = '/dictionary_api'
+
+    @classmethod
+    def serve(cls, req):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+        jp.Div(a=div, text='Instant English Dictionary', classes='text-4xl m-2')
+        jp.Div(a=div, text='Get the definition of any English word instantly as you type',
+               classes='text-lg')
+        input_div = jp.Div(a=div, classes='grid grid-cols-2')
+
+        input_box = jp.Input(a=input_div, placeholder='Type in a word here', classes='m-2 bg-gray-100 border-2 '
+                                                                                     'border-black-500 rounded w-64 '
+                                                                                     'focus:outline-none '
+                                                                                     'py-2 px-4 focus:bg-white')
+        output_div = jp.Div(a=div, classes='m-2 p-2 text-lg border-2 h- bg-gray-100')
+        jp.Button(a=input_div, text='Get Definition', click=cls.get_definition, outputdiv=output_div,
+                  classes='border- text-black-500 m-2 p-2 bg-gray-300', inputbox=input_box)
+
+        return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        # defined = Definition(widget.inputbox.value).get()
+        req = requests.get(f"http://127.0.0.1:8000/api?w={widget.inputbox.value}")
+        data = req.json()
+        widget.outputdiv.text = '\n'.join(data['definition'])
+        # A static method is a method inside the class that behaves
+        # like a function
--- venv/webapp/main.py
+++ venv/webapp/main.py
@@ -2,9 +2,11 @@ import justpy as jp
 from about import About
 from home import Home
 from dictionary import Dictionary
+from dictionary_api import Dictionary as Dict
 
 
 jp.Route(About.path, About.serve)
 jp.Route(Home.path, Home.serve)
 jp.Route(Dictionary.path, Dictionary.serve)
+jp.Route(Dict.path, Dict.serve)
 jp.justpy(port=8001)
